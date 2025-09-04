aadb1719c5a24496c5216220cb539b067a415a5e
lassulus <git@lassul.us>
2025-09-04 16:06:17 +0200

    add whacky ass install package

--- flake.nix
+++ flake.nix
@@ -12,5 +12,13 @@
       ];
     };
     packages.liveCD.x86_64-linux = self.outputs.nixosConfigurations.streamdesk.config.system.build.images.iso;
+    packages.default.install = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (arch: nixpkgs.legacyPackages.${arch}.writeScriptBin "install" ''
+      #!/bin/sh
+      git clone https://github.com/andir/nixcon-streaming-config nixcon-streaming-config
+      cd nixcon-streaming-config
+      sudo nixos-generate-config --fo-filesystems --show-hardware-config > hardware-configuration.nix
+      git add hardware-configuration.nix
+      sudo nix --extra-experimental-features 'nix-command flakes' run github:nix-community/disko#disk-install -- --flake .#streamdesk --disk main /dev/nvme0n1
+    '');
   };
 }
