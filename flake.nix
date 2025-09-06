bcd5b73d63fc15191ff7cd4f29110be39ca46af0
Picnoir <picnoir@alternativebit.fr>
2025-09-06 08:04:38 +0200

    Fix lassulus's nonsense

--- flake.nix
+++ flake.nix
@@ -16,9 +16,9 @@
       #!/bin/sh
       git clone https://github.com/andir/nixcon-streaming-config nixcon-streaming-config
       cd nixcon-streaming-config
-      sudo nixos-generate-config --fo-filesystems --show-hardware-config > hardware-configuration.nix
+      sudo nixos-generate-config --no-filesystems --show-hardware-config > hardware-configuration.nix
       git add hardware-configuration.nix
-      sudo nix --extra-experimental-features 'nix-command flakes' run github:nix-community/disko#disk-install -- --flake .#streamdesk --disk main /dev/nvme0n1
+      sudo nix --extra-experimental-features 'nix-command flakes' run github:nix-community/disko#disko-install -- --flake .#streamdesk --disk main /dev/nvme0n1
     '');
   };
 }
