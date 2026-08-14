5f62d7393bd9bd1f35722fb2d661d204e6360a45
Andreas Rammhold <andreas@rammhold.de>
2026-08-14 11:34:58 +0200

    Prometheus is now on the VM

--- streamdesk/grafana.nix
+++ streamdesk/grafana.nix
@@ -2,7 +2,7 @@
 
 {
   services.prometheus = {
-    enable = true;
+    enable = false;
     port = 9090;
     listenAddress = "127.0.0.1";
     retentionTime = "30d";
@@ -10,7 +10,7 @@
     exporters.node = {
       enable = true;
       port = 9100;
-      listenAddress = "127.0.0.1";
+      listenAddress = "[::]";
       enabledCollectors = [
         "systemd"
         "processes"
@@ -27,53 +27,6 @@
         "sysctl"
       ];
     };
-
-    scrapeConfigs = [
-      {
-        job_name = "node";
-        static_configs = [{
-          targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
-        }];
-      }
-    ];
   };
-
-  services.grafana = {
-    enable = true;
-
-    settings = {
-      server = {
-        http_addr = "::";
-        http_port = 3000;
-        domain = "localhost";
-      };
-      security.secret_key = "whatevaerasdfasdfsadf";
-      "auth.anonymous" = {
-        enabled = true;
-        org_name = "Main Org.";
-        org_role = "Viewer";
-        hide_version = true;
-      };
-
-      users = {
-        allow_sign_up = false;
-        allow_org_create = false;
-        viewers_can_edit = false;
-        default_theme = "dark";
-      };
-
-      dashboards.default_home_dashboard_path = "${./node-exporter-dashboard.json}";
-
-      analytics.reporting_enabled = false;
-      snapshots.external_enabled = false;
-    };
-
-    provision.datasources.settings.datasources = [{
-      name = "Prometheus";
-      type = "prometheus";
-      uid = "prometheus";
-      url = "http://127.0.0.1:${toString config.services.prometheus.port}";
-      isDefault = true;
-    }];
-  };
+  networking.firewall.allowedTCPPorts = [ 9100 ];
 }
